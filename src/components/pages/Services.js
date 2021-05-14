dea3dedda2c2f750302390caf0d4cecfe2489258
Ayush <ayush.goutam23@gmail.com>
2021-05-14 23:35:50 +0530

    Ser updated

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,6 +3,21 @@ import '../../App.css';
 import '../Cards.css';
 import './Servicess.css';
 import CardItem from '../CardItem';
+import Skydiving from '../.././images/skydiving.jpg'
+import Rafting from '../.././images/rafting.jpg'
+import Racing from '../.././images/racing.jpg'
+import Bungy from '../.././images/bungy.jpg'
+import Sandboarding from '../.././images/sandboarding.jpg'
+import Rapelling from '../.././images/rapelling.jpg'
+import Caving from '../.././images/caving.jpg'
+import Paragliding from '../.././images/paragliding.jpg'
+import Rockclimbing from '../.././images/rockclimbing.jpg'
+import Hanggliding from '../.././images/hanggliding.jpg'
+import Skyswinging from '../.././images/skyswinging.jpg'
+import Parasailing from '../.././images/parasailing.jpg'
+import Biking from '../.././images/biking.jpg'
+import Skiing from '../.././images/skiing.jpg'
+import Surfing from '../.././images/surfing.jpg'
 export default function Services() {
   return (
     <>
@@ -11,19 +26,19 @@ export default function Services() {
     <div className='cards__wrapper'>
     <ul className='cards__items'>
             <CardItem
-              src='./images/skydiving.jpg'
+              src={Skydiving}
               text='launching yourself out of an airplane and experience a breathtaking freefall.'
               label='Sky Diving'
               path='/'
             />
             <CardItem
-              src='./images/rafting.jpg'
+              src={Rafting}
               text="Grab a paddle, hop in an inflatable raft and explore world's most turbulent waters."
               label='Rafting'
               path='/'
             />
             <CardItem
-              src='./images/racing.jpg'
+              src={Racing}
               text='Live out your motoring fantasies in style, with an Exotic Car Driving Experiences.'
               label='Car Racing'
               path='/'
@@ -33,19 +48,19 @@ export default function Services() {
     <div className='cards__wrapper'>
     <ul className='cards__items'>
             <CardItem
-              src='./images/bungy.jpg'
+              src={Bungy}
               text='Ultimate death-defying plunge with your nose skimming the floor or a short-and-sweet drop.'
               label='Bungy jumping'
               path='/'
             />
             <CardItem
-              src='./images/sandboarding.jpg'
+              src={Sandboarding}
               text='Most entertaining way to get around in the desert is by sliding downhill on a sandboard.'
               label='Sandboarding'
               path='/'
             />
             <CardItem
-              src='./images/rapelling.jpg'
+              src={Rapelling}
               text='Even more thrilling than climbing up a mountain, is rappelling, or abseiling.'
               label='Rappelling'
               path='/'
@@ -55,19 +70,19 @@ export default function Services() {
     <div className='cards__wrapper'>
     <ul className='cards__items'>
             <CardItem
-              src='./images/caving.jpg'
+              src={Caving}
               text='Crawling through narrow rock crevices and scrambling through pitch black caves.'
               label='Caving'
               path='/'
             />
             <CardItem
-              src='./images/paragliding.jpg'
+              src={Paragliding}
               text='Experience the feeling of floating in mid-air with a tandem paragliding excursion.'
               label='Paragliding'
               path='/'
             />
             <CardItem
-              src='./images/rockclimbing.jpg'
+              src={Rockclimbing}
               text='Achievement of scaling a precipitous cliff-face with nothing but your bare hands.'
               label='Rock climbing'
               path='/'
@@ -77,19 +92,19 @@ export default function Services() {
     <div className='cards__wrapper'>
     <ul className='cards__items'>
             <CardItem
-              src='./images/hanggliding.jpg'
+              src={Hanggliding}
               text='Dive like a bird on a hand gliding excursion with nothing beneath you but fresh air.'
               label='Hang gliding'
               path='/'
             />
             <CardItem
-              src='./images/skyswinging.jpg'
+              src={Skyswinging}
               text='Make like a modern-day Tarzan, hanging from a harness and soaring through the jungle.'
               label='Sky swinging'
               path='/'
             />
             <CardItem
-              src='./images/parasailing.jpg'
+              src={Parasailing}
               text='If you prefer water to heights, ditch the paragliding and go parasailing instead.'
               label='Parasailing'
               path='/'
@@ -99,19 +114,19 @@ export default function Services() {
     <div className='cards__wrapper'>
     <ul className='cards__items'>
             <CardItem
-              src='./images/biking.jpg'
+              src={Biking}
               text='Forget rolling through country lanes and leisurely pedaling the beachfront.'
               label='Extreme biking'
               path='/'
             />
             <CardItem
-              src='./images/skiing.jpg'
+              src={Skiing}
               text="There's an array of beginner runs, near-vertical black runs and challenging off-piste routes."
               label='Skiing'
               path='/'
             />
             <CardItem
-              src='./images/surfing.jpg'
+              src={Surfing}
               text="Now's the time to learn to catch some waves for yourself and wrestle with the waves."
               label='Surfing'
               path='/'
